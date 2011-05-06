9ae0a649fb4d70c604fe44e023615a1f14548081
Jun Tanaka <junpluse@gmail.com>
2011-05-06 19:08:07 +0900

    Initial Commit

---
+++ JTListView/JTListViewAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  JTListViewAppDelegate.h
+//  JTListView
+//
+//  Created by Jun on 5/6/11.
+//  Copyright 2011 東京工科大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JTListViewAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ JTListView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  JTListView
+//
+//  Created by Jun on 5/6/11.
+//  Copyright 2011 東京工科大学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
